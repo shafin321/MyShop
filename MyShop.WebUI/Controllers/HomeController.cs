563ebbaf34462d9873a5b7c5fc8f8b4148de133d
shafin321 <shafinhaque91@gmail.com>
2020-12-01 21:46:16 +0100

    Fixed viewIndex and Details

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +10,34 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> _contextProduct;
+        IRepository<ProductCategory> _contextCategory;
+
+        public HomeController(IRepository<Product> context, IRepository<ProductCategory> contextCategory)
+        {
+            _contextProduct = context;
+            _contextCategory = contextCategory;
+        }
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _contextProduct.GetAll();
+            return View(products);
+        }
+
+        public ActionResult Details(string id)
+        {
+            Product product = _contextProduct.Find(id);
+           
+            
+                return View(product);
+            
+
+         
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
